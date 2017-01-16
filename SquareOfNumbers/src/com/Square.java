f96509d3de6125468a4c81be2f4f8721031d2e82
vivekshiva4 <vivekshiva4@gmail.com>
2017-01-16 22:56:02 +0530

    Add files via upload

---
+++ SquareOfNumbers/src/com/Square.java
@@ -0,0 +1,17 @@
+package com;
+
+public class Square {
+
+	public static void main(String[] args) {
+		int x;
+		int y;
+		System.out.println("squre of numbers upto 25 are :");
+		for(x=1;x<=25;x++)
+		{
+			y=x*x;
+		
+			System.out.println(y);
+		}
+	}
+
+}
