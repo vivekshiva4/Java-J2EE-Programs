501624ccb39b657966b200a971005ca8389e09c0
vivekshiva4 <vivekshiva4@gmail.com>
2017-01-16 22:59:16 +0530

    Add files via upload

---
+++ WraperClasses/src/Unwrapping.java
@@ -0,0 +1,12 @@
+
+public class Unwrapping {
+
+	public static void main(String[] args) {
+		int i=6;
+		Integer a=new Integer(i);
+		int j=a.intValue();
+		System.out.println(j);
+
+	}
+
+}
---
+++ WraperClasses/src/Wraper.java
@@ -0,0 +1,10 @@
+
+public class Wraper {
+
+	public static void main(String[] args) {
+		int x=5;
+		Integer a=new Integer(x);//wrapping of primitive data type into bject
+		System.out.println(x);
+	}
+
+}
